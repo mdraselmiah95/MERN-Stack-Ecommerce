bb0b6cc0454c9cd3c3174f8a86a4096638f1e17e
mdraselmiah95 <russelroy15@gmail.com>
2023-04-18 05:14:41 +0600

    MERN Stack Ecommerce backend resetPassword

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -65,4 +65,18 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Generate password rest token
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+  return resetToken;
+};
+
 module.exports = mongoose.model("User", userSchema);
