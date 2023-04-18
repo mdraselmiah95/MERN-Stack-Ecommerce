392d7e60713ca535417751c758f6f932d6c0d39d
mdraselmiah95 <russelroy15@gmail.com>
2023-04-19 03:42:03 +0600

    WIP on MERN Stack Get ALl products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,6 @@
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Product = require("../models/productModel");
+const ApiFeatures = require("../utils/apiFeatures");
 const ErrorHandler = require("../utils/errorHandler");
 
 // Create Product -- Admin
@@ -10,5 +11,26 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   res.status(201).json({
     success: true,
     product,
+  });
+});
+
+// Get all products
+exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
+  const productCounts = await Product.countDocuments();
+  const resultPerPage = 10;
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+
+  const products = await apiFeatures.query;
+  const filteredProductsCount = await products.length;
+
+  res.status(200).json({
+    success: true,
+    productCounts,
+    resultPerPage,
+    filteredProductsCount,
+    products,
   });
 });
