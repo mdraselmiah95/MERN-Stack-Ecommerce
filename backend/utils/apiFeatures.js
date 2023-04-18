4205ee47c7985d0fd2c184cc4d590a9afd24fa8b
mdraselmiah95 <russelroy15@gmail.com>
2023-04-19 03:32:27 +0600

    WIP on MERN Stack Product advance finding

---
+++ backend/utils/apiFeatures.js
@@ -0,0 +1,40 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  search() {
+    const keyword = this.queryString.keyword
+      ? {
+          name: {
+            $regex: this.queryString.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+  filter() {
+    const queryCopy = { ...this.queryString };
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
+    let queryString = JSON.stringify(queryCopy);
+    queryString = queryString.replace(
+      /\b(gt|gte|lt|lte)\b/g,
+      (match) => `$${match}`
+    );
+
+    this.query = this.query.find(JSON.parse(queryString));
+    return this;
+  }
+  pagination(resultPerPage) {
+    const currentPage = Number(this.queryString.page) || 1;
+    const skip = resultPerPage * (currentPage - 1);
+    this.query = this.query.limit(resultPerPage).skip(skip);
+    return this;
+  }
+}
+
+module.exports = ApiFeatures;
