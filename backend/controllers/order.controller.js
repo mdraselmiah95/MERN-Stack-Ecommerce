f3e2179a17bd75f254922cd02db1208337b01141
mdraselmiah95 <russelroy15@gmail.com>
2023-04-20 03:29:36 +0600

    WIP on MERN Stack Order Controller

---
+++ backend/controllers/order.controller.js
@@ -0,0 +1,50 @@
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Order = require("../models/order.model");
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+
+// Create new Order
+exports.newOrder = catchAsyncErrors(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user._id,
+  });
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+
+// get Single Order
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
