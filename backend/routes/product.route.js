f332745415a02d061d51bfcc3c781ec3b2e6cb03
mdraselmiah95 <russelroy15@gmail.com>
2023-04-19 06:35:02 +0600

    WIP on MERN Stack Create Product

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,12 +2,35 @@ const express = require("express");
 const {
   createProduct,
   getAllProducts,
+  updateProduct,
+  deleteProduct,
+  getSingleProduct,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../controllers/product.controller");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 
-router.route("/admin/product/new").post(createProduct);
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/product/:id").get(getSingleProduct);
+
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
