efd4d6925ed2fda904800e0bc31cd23aab8eb510
mdraselmiah95 <russelroy15@gmail.com>
2023-04-21 03:42:56 +0600

    WIP on MERN Stack Client assets setup

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,3 +1,39 @@
-const assets = {};
+import logo from "./images/logo.svg";
+import cat_1 from "./images/category/cat-1.png";
+import cat_2 from "./images/category/cat-2.png";
+import cat_3 from "./images/category/cat-3.png";
+import cat1 from "./images/category/cat1.png";
+import cat2 from "./images/category/cat2.png";
+import cat3 from "./images/category/cat3.jpg";
+import cat4 from "./images/category/cat4.png";
+import cat5 from "./images/category/cat5.png";
+import cat6 from "./images/category/cat6.png";
+import cat7 from "./images/category/cat7.png";
+import cat8 from "./images/category/cat8.png";
+import cat9 from "./images/category/cat9.png";
+import cat10 from "./images/category/cat10.png";
+import cat11 from "./images/category/cat11.png";
+
+const assets = {
+  images: {
+    logo,
+    catImage: {
+      cat_1,
+      cat_2,
+      cat_3,
+      cat1,
+      cat2,
+      cat3,
+      cat4,
+      cat5,
+      cat6,
+      cat7,
+      cat8,
+      cat9,
+      cat10,
+      cat11,
+    },
+  },
+};
 
 export default assets;
