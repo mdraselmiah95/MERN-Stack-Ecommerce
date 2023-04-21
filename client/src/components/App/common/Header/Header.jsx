5b5353f05d60dece470b4b9557349255b30b2c01
mdraselmiah95 <russelroy15@gmail.com>
2023-04-21 06:32:11 +0600

    basic scss added

--- client/src/components/App/common/Header/Header.jsx
+++ client/src/components/App/common/Header/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Head from "./Head";
+import Search from "./Search";
+import Navbar from "./Navbar";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Head />
+      <Search />
+      <Navbar />
+    </div>
+  );
 };
 
 export default Header;
