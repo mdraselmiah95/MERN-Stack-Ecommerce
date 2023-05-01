2c3b6a894f2a11a0a74d7c4c0d79ce06692e455a
mdraselmiah95 <russelroy15@gmail.com>
2023-05-01 10:43:37 +0600

    WIP on MERN project CLient navbar

--- client/src/components/App/common/Header/Navbar.jsx
+++ client/src/components/App/common/Header/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <header className="header">
+        <div className="container d_flex">
+          <div className="catagories d_flex">
+            <span className="fa-solid fa-border-all" />
+            <h4>
+              Categories <i className="fas fa-caret-down" />
+            </h4>
+          </div>
+          <div className="navlink">
+            <ul className="link f_flex capitalize">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/products">Products</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Navbar;
