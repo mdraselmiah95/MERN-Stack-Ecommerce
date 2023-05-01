dd2b5cd205c5053d20e036ea4e68ff2b220584f6
mdraselmiah95 <russelroy15@gmail.com>
2023-05-01 19:28:22 +0600

    WIP on Project Flash Deals

--- client/src/components/App/flashDeals/FlashCard.jsx
+++ client/src/components/App/flashDeals/FlashCard.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Rating from "@mui/material/Rating";
 
-const FlashCard = () => {
-  return <div>FlashCard</div>;
+const FlashCard = ({ product }) => {
+  console.log(product);
+  return (
+    <div className="product_container ">
+      <div className="product mtop">
+        <div className="img">
+          <span className="discount">% Off</span>
+          <img src={product?.cover} alt="product image" />
+        </div>
+        <div className="product-details">
+          <h3>{product?.name}</h3>
+          <Rating name="half-rating" defaultValue={4.6} precision={0.5} />
+          <div className="price">
+            <h4>${product?.price}.00 </h4>
+            <button>
+              <i className="fa fa-plus" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FlashCard;
--- client/src/components/App/flashDeals/index.jsx
+++ client/src/components/App/flashDeals/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import FlashCard from "./FlashCard";
+import Data from "../../../constants/Data";
 
 const FlashDeals = () => {
-  return <div>FlashDeals </div>;
+  return (
+    <section className="flash">
+      <div className="container">
+        <div className="heading f_flex">
+          <i className="fa fa-bolt" />
+          <h1> Best Deals</h1>
+        </div>
+        <div className="d_flex">
+          {Data?.productItems?.map((item) => {
+            return <FlashCard key={item.id} product={item} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FlashDeals;
