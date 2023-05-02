83470c15bf134488af1595b13eb9869146435505
mdraselmiah95 <russelroy15@gmail.com>
2023-05-02 21:30:20 +0600

    WIP on MERN Project get all products

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Hero from "../../components/App/Hero";
 import FlashDeals from "../../components/App/flashDeals";
+import { getAllProducts } from "../../redux/actions/ProductAction";
 
 const Home = () => {
+  const { productsInfo } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productsInfo.length === 0) {
+      dispatch(getAllProducts());
+    }
+  }, [productsInfo]);
+
   return (
     <>
       <Hero />
