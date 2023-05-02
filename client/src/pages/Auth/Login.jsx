4235853e8f16d4ea610cda60b76f59d3fc6f768f
mdraselmiah95 <russelroy15@gmail.com>
2023-05-02 18:57:18 +0600

    WIP on MERN Project register

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,7 +1,30 @@
-import { Stack } from "@mui/material";
-import React from "react";
+import {
+  Alert,
+  Button,
+  CircularProgress,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useRef } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import WHiteSpace from "../../components/App/whitespac/WHiteSpace";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = () => {
+    const user = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    console.log(user);
+  };
+
   return (
     <Stack>
       <Stack
@@ -27,6 +50,56 @@ const Login = () => {
           pt={5}
         >
           <h2>Login</h2>
+
+          <TextField
+            label="Email"
+            type="email"
+            variant="outlined"
+            inputRef={emailRef}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            inputRef={passwordRef}
+          />
+          <WHiteSpace height={10} />
+
+          <Button variant="contained" onClick={handleSubmit}>
+            {/* {isLoading ? (
+              <CircularProgress size={25} color="warning" />
+            ) : (
+              "Login"
+            )} */}
+            Login
+          </Button>
+
+          <Typography variant="p">
+            Forgot Password?
+            <Typography
+              sx={{
+                textDecoration: "underline",
+                display: "inline-block",
+                marginLeft: "5px",
+              }}
+            >
+              <Link to="/forgot-password">Forgot Password</Link>
+            </Typography>
+          </Typography>
+
+          <Typography variant="p">
+            Don&#39;t have an account?
+            <Typography
+              sx={{
+                textDecoration: "underline",
+                display: "inline-block",
+                marginLeft: "5px",
+              }}
+            >
+              <Link to="/register">Register</Link>
+            </Typography>
+          </Typography>
+          <WHiteSpace height={50} />
         </Stack>
       </Stack>
     </Stack>
--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,7 +1,118 @@
-import React from "react";
+import {
+  Alert,
+  Button,
+  CircularProgress,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import WHiteSpace from "../../components/App/whitespac/WHiteSpace";
 
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const handleSubmit = () => {
+    const data = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    console.log(data);
+  };
+  return (
+    <Stack>
+      <Stack
+        sx={{
+          width: "40%",
+          height: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "auto",
+          borderRadius: "5px",
+          my: 4,
+          boxShadow: 2,
+        }}
+      >
+        <Stack
+          direction="column"
+          spacing={2}
+          sx={{
+            width: "60%",
+            textAlign: "center",
+          }}
+          pt={5}
+        >
+          <h2>Register</h2>
+          {/* {error && <Alert severity="error">{error}</Alert>} */}
+          <TextField
+            label="Name"
+            type="text"
+            variant="outlined"
+            inputRef={nameRef}
+          />
+          <TextField
+            label="Email"
+            type="email"
+            variant="outlined"
+            inputRef={emailRef}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            inputRef={passwordRef}
+          />
+          <Button variant="contained" component="label" color="warning">
+            Upload Avatar
+            <input
+              type="file"
+              hidden
+              // onChange={(e) => {
+              //   setAvatar(e.target.files[0]);
+              //   const reader = new FileReader();
+              //   reader.readAsDataURL(e.target.files[0]);
+              //   reader.onload = () => {
+              //     setImage(reader.result);
+              //   };
+              // }}
+            />
+          </Button>
+          {/* {image && (
+            <img src={image} alt="avatar" width="100px" height="100px" />
+          )} */}
+          <WHiteSpace height={20} />
+          <Button variant="contained" onClick={handleSubmit}>
+            {/* {isLoading ? (
+              <CircularProgress color="warning" size={22} />
+            ) : (
+              "Register"
+            )} */}
+            Register
+          </Button>
+
+          <Typography variant="p">
+            Already have an account?
+            <Typography
+              sx={{
+                textDecoration: "underline",
+                display: "inline-block",
+                marginLeft: "5px",
+              }}
+            >
+              <Link to="/login">Login</Link>
+            </Typography>
+          </Typography>
+          <WHiteSpace height={50} />
+        </Stack>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Register;
